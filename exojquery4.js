97683a30c6d8ad84655820d687f69c593e19fe8b
Chagnaud <chagnaud.jerome@gmail.com>
2019-04-17 12:01:41 +0200

    commit2

---
+++ exojquery4.js
@@ -0,0 +1,18 @@
+/*jQuery(function ($){
+    //Récupération (getter) du texte de l'élément #h1
+    let titre1 = $("#h1").text();
+    console.log(titre1);
+
+//Modification (setter) du texte de l'élément #h1
+//$("#h1").text("Nouveau titre de niveau 1");
+
+
+});
+*/
+
+jQuery(function ($){
+    //Récupération (getter) du html de l'élément #h1
+    let titre1 = $("#h1").text();
+    console.log(titre1);
+
+});
